0934d3c1cd01577d5f5c849c2bfde1d1f3068d75
Giorgi Dogadze <g.dogadze@gmail.com>
2022-09-26 10:58:59 +0400

    feat: create isPrime function

---
+++ isPrime.js
@@ -0,0 +1,21 @@
+var prompt = require("prompt-sync")();
+
+const isPrime = () => {
+  let counter = 0;
+  const num = prompt("Input a number ");
+
+  for (let i = 1; i <= num; i++) {
+    if (num % i == 0) {
+      counter++;
+    }
+  }
+
+  console.log("counter", counter);
+  if (counter == 2) {
+    console.log("true");
+  } else {
+    console.log("false");
+  }
+};
+
+isPrime();
