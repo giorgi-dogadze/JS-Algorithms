f4d90432085c7f764d104b00d647490add98eaab
Giorgi Dogadze <g.dogadze@gmail.com>
2023-03-23 23:59:12 +0400

    feat: implement search method for BinaryTree

--- Data Structures/binarySearchTree.js
+++ Data Structures/binarySearchTree.js
@@ -29,23 +29,47 @@ class BinaryTree {
     if (this.isEmpty()) {
       this.root = node;
     } else {
-      this.insertNode(this.root, newNode);
+      this.insertNode(this.root, node);
     }
   }
 
   insertNode(currentNode, newNode) {
     if (newNode.value < currentNode.value) {
       if (currentNode.left === null) {
-        currentNode.left = node;
+        currentNode.left = newNode;
       } else {
         this.insertNode(currentNode.left, newNode);
       }
     } else {
       if (currentNode.right === null) {
-        currentNode.right = node;
+        currentNode.right = newNode;
       } else {
         this.insertNode(currentNode.right, newNode);
       }
     }
   }
+
+  search(root, value) {
+    if (!root) {
+      return false;
+    } else {
+      console.log("root.value", root.value);
+      if (root.value === value) {
+        return true;
+      } else if (value < root.value) {
+        return this.search(root.left, value);
+      } else {
+        return this.search(root.right, value);
+      }
+    }
+  }
 }
+
+const tree = new BinaryTree();
+
+tree.insert(10);
+tree.insert(5);
+tree.insert(1);
+tree.insert(13);
+
+console.log("tree.search(15)", tree.search(tree.root, 1));
